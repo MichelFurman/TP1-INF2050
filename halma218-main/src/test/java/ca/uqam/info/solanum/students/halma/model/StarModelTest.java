125e8ce4ff3b5e20d8163524ae8dff0c810917c6
Andrei-Panait <hh790815@ens.uqam.ca>
2025-02-01 17:55:47 -0500

    tests fixed

--- halma218-main/src/test/java/ca/uqam/info/solanum/students/halma/model/StarModelTest.java
+++ halma218-main/src/test/java/ca/uqam/info/solanum/students/halma/model/StarModelTest.java
@@ -1,62 +1,22 @@
 package ca.uqam.info.solanum.students.halma.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import ca.uqam.info.solanum.students.halma.model.ModelImpl;
+import ca.uqam.info.solanum.inf2050.f24halma.model.AbstractModelTest;
 import ca.uqam.info.solanum.inf2050.f24halma.model.Model;
-import ca.uqam.info.solanum.inf2050.f24halma.model.Field;
-import ca.uqam.info.solanum.students.halma.model.BoardImpl;
+import ca.uqam.info.solanum.students.halma.controller.ModelFactoryImpl;
 
-import java.util.Set;
+/**
+ * This class must be implemented by students.
+ */
+public class StarModelTest extends AbstractModelTest {
 
-class StarModelTest {
 
-  @Test
-  void testGetPlayerNames() throws Exception {
-    String[] playerNames = {"Max", "Maram", "Roman"};
-    Model model = new ModelImpl(2, playerNames);
+  @Override
+  public Model getModel(int baseSize) {
 
-    // Test that the player names are correctly returned
-    assertArrayEquals(playerNames, model.getPlayerNames());
-  }
+    // TODO: Provide new Model object, using factory (your implementation).
+    // Example (your factory /model constructor may be different)
+    // return new ModelFactory().createModel(baseSize, new String[] {"Max", "Ryan", "Quentin"});
+    return new ModelFactoryImpl(baseSize, new String[] {"Max", "Ryan", "Quentin"}).createModel(baseSize, new String[] {"Max", "Ryan", "Quentin"});
 
-  @Test
-  void testGetBoard() throws Exception {
-    String[] playerNames = {"Max", "Maram", "Roman"};
-    Model model = new ModelImpl(2, playerNames);
-
-    // Test that the board is not null
-    assertNotNull(model.getBoard());
-  }
-
-  @Test
-  void testGetCurrentPlayer() throws Exception {
-    String[] playerNames = {"Max", "Maram", "Roman"};
-    Model model = new ModelImpl(2, playerNames);
-
-    // Test that the current player index is correctly initialized
-    assertEquals(0, model.getCurrentPlayer());
-  }
-
-  @Test
-  void testGetPlayerFields() throws Exception {
-    String[] playerNames = {"Max", "Maram", "Roman"};
-    Model model = new ModelImpl(2, playerNames);
-
-    // Test that the player fields are initially empty
-    Set<Field> fields = model.getPlayerFields(0);
-    assertNotNull(fields);
-    assertTrue(fields.isEmpty());
-  }
-
-  @Test
-  void testGetHomeFields() throws Exception {
-    String[] playerNames = {"Max", "Maram", "Roman"};
-    Model model = new ModelImpl(2, playerNames);
-
-    // Test that the home fields are correctly returned
-    Set<Field> homeFields = model.getBoard().getAllHomeFields();
-    assertNotNull(homeFields);
-    assertFalse(homeFields.isEmpty());
   }
 }
